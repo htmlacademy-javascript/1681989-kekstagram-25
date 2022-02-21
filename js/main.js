78d52cf7ad6884e6b28dd183c4069a2aed6ad109
Roman <rmn.styazhkin@gmail.com>
2022-02-21 11:09:51 +0300

    исправляю домашку

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandomInteger(min = 0, max = 1) {
+const getRandomInteger = (min = 0, max = 1) => {
   min = +min;
   max = +max;
   const isWrongRange = min >= max || min < 0 || max < 0 || isNaN(min) || isNaN(max);
@@ -8,17 +8,17 @@ function getRandomInteger(min = 0, max = 1) {
   }
 
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
 getRandomInteger(0, 3);
 
-function checkStrLength (string = '', maxLength = 10) {
+const checkStrLength = (string = '', maxLength = 10) => {
   string = string.toString();
   if (string.length > maxLength) {
     return false;
   }
 
   return true;
-}
+};
 
 checkStrLength('some value', 10);
