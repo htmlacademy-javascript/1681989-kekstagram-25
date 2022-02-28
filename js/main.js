5d54433306a5f07a29490736f1537f47397e60ee
Roman <rmn.styazhkin@gmail.com>
2022-02-28 16:30:16 +0300

    мучаюсь с айдишниками

--- js/main.js
+++ js/main.js
@@ -16,4 +16,51 @@ const checkStrLength = (string = '', maxLength = 10) => {
 
 checkStrLength('some value', 11);
 
-// task
+// // task
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.',
+  'Как можно было поймать такой неудачный момент?!',
+];
+
+const names = ['Артем', 'Филипп', 'Яна', 'Анастасия'];
+
+
+function foo (number) {
+  const arr = [];
+  for (let i = 1; i <= 4; i++) {
+    const randomComment = {
+      id: i + number,
+      avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+      message: COMMENTS[getRandomInteger(0, COMMENTS.length - 1)],
+      name: names[getRandomInteger(0, names.length - 1)],
+    };
+    arr.push(randomComment);
+  }
+
+  return arr;
+}
+
+function generateArr () {
+  const result = [];
+  for (let i = 1; i <= 5; i++) {
+    const randomObj = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: 'Случайное описание для фотографии',
+      likes: getRandomInteger(15, 200),
+      comments: foo(i),
+    };
+
+    result.push(randomObj);
+  }
+
+  return result;
+}
+
+console.log(generateArr());
+
