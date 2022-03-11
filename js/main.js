63e3e54950cdd4ffa993f8c91b964b1e56ceb658
Roman <rmn.styazhkin@gmail.com>
2022-03-11 14:55:34 +0300

    передлываю домашку

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
 import { generateArr } from './data.js';
-import { renderImages, imgContainer} from './render-img-1.js';
-import { openInfoImg, btnClose } from './info-modal.js';
-import { closeModal } from './toggle-modal.js';
+import { renderImages, imgContainer} from './render-img.js';
 
 
 const generatedImgData = generateArr(25);
 
 renderImages(generatedImgData);
-
-imgContainer.addEventListener('click', (e) => {
-  e.preventDefault();
-  openInfoImg(e, generatedImgData);
-});
-
-btnClose.addEventListener('click', closeModal);
